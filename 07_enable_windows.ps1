e6673d6c3062141296cb6ff8e68cd4f41b91490c
Jere Vekka <jere.vekka@gmail.com>
2021-09-17 15:30:10 +0300

    Updates for day 7 and 8

---
+++ 07_enable_windows.ps1
@@ -0,0 +1,4 @@
+# Script that configures all necessary changes for Ansible
+# https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1
+# -EnableCredSSP
+# -DisableBasicAuth
